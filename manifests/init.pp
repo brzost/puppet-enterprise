7aa5d92c114849342b15e6380ce93715f820bec4
brzost <brzost@gmail.com>
2023-01-31 09:19:03 +0000

    configured module

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,3 +1,14 @@
-class apache {
-  include apache::install
+class apache (
+  String $install_name,
+  Array $config_paths,
+  String $service_name,
+  String $vhost_dir,
+) {
+  contain apache::install
+  contain apache::config
+  contain apache::service
+
+  Class['::apache::install']
+  -> Class['::apache::config']
+  ~> Class['::apache::service']
 }
